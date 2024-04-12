f6987b1c79efedc8bf8149be8cc9efe04e0539c2
Rosan7 <85017693+Rosan7@users.noreply.github.com>
2024-04-12 12:31:51 +0530

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -36,46 +36,12 @@ def findifAisB(A,B,E):
         else:
             return False
 
-# A = [1,2,3,4,5]
-# E = 1
-# B = [2,1,6,4,3]
-# print(findifAisB(A,B,E))
-
-from collections import defaultdict
+A = [1,2,3,4,5]
+E = 1
+B = [2,1,6,4,3]
+print(findifAisB(A,B,E))
 
 
-
-
-
-
-
-def findmaxlength(A,N,target):
-    idx_sum = defaultdict(lambda : 0)
-    curr_sum = 0
-    soln = []
-    maxlength = 0
-    start = 0
-    end = 0
-    for i in range(len(A)):
-        curr_sum += A[i]
-
-        if curr_sum == target:
-
-            maxlength = max(maxlength,i+1)
-
-        if curr_sum-k in idx_sum:
-            start = idx_sum[curr_sum-target] + 1
-            end = i
-            maxlength = max(maxlength,end-start+1)
-        idx_sum[curr_sum] = i
-
-    return maxlength
-
-
-A = [10,2,-2,-20,10]
-k = -10
-N = 5
-print(findmaxlength(A,N,k))
 
 
 
